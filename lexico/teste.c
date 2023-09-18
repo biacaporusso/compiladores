20e85f854b50cf91d7e439a16acb160603629bfb
Beatriz <beatriz.caporusso@hotmail.com>
2023-09-18 17:23:18 -0300

    lexico

---
+++ lexico/teste.c
@@ -0,0 +1,7 @@
+int main()
+{
+    int identificador_09878;
+    int _123;
+    int identificador_09_muito_grande_78_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx;
+    return 0;
+}
