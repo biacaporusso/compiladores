bf0e6cf20fe90250ff0f2788da4ec96b07f297a5
Beatriz <beatriz.caporusso@hotmail.com>
2023-09-19 17:02:40 -0300

    9/10 testes

--- lexico/teste.c
+++ lexico/teste.c
@@ -1,7 +1,5 @@
-int main()
+if(1)
 {
-    int identificador_09878;
-    int _123;
-    int identificador_09_muito_grande_78_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx;
-    return 0;
-}
+    /* isto eh um
+    comentario iniciado
+    e nao terminado */
